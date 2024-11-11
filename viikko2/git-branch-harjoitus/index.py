f29873d874c46b6c9aaa954f77449ace3a1ab4a9
Toivo Närhi <toivo.narhi@helsinki.fi>
2024-11-11 21:00:32 +0200

    Muu

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -8,8 +8,8 @@ logger("aloitetaan") #muutos mainiin
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+print(f"{summa(x, y)}") #muutos
+print(f"{erotus(x, y)}") #muutos
 
 logger("lopetetaan")
 
